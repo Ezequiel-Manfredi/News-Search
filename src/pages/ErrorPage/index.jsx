da2d37d0507d4ee07c0727945faa4d7af4d959e9
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-07-19 18:06:50 -0300

    [add]: error styles

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,8 +1,18 @@
+import { Typography, Alert, AlertTitle } from '@mui/material'
+
 export default function ErrorPage() {
     return (
         <>
-            <h1>Error 404</h1>
-            <p>Recurso no encontrado</p>
+            <Alert severity="error" className=''>
+                <AlertTitle>
+                    <Typography variant="h1" component="h2" className='text-center'>
+                        Error 404
+                    </Typography>
+                </AlertTitle>
+                <Typography variant="h5" component="p" className='text-center'>
+                    OOPS!!! Recurso no encontrado
+                </Typography>
+            </Alert>
         </>
     )
 }
