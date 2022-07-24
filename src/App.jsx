6ab37bde7b7c9f056a56e499305134d9ffcc68e2
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-07-24 16:12:48 -0300

    [change]: adapted to the logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { BrowserRouter, Navigate, Route, Routes, Link } from 'react-router-dom'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import ErrorPage from './pages/ErrorPage'
 import SearchPage from './pages/SearchPage'
 
 export default function App() {
     return (
         <BrowserRouter>
-            <Box component='header' className='max-w-[100vw] h-[10vh] border-b pb-4'>
-                <Link to={"/"}>
-                    <img className='h-full' src="https://upload.wikimedia.org/wikipedia/commons/4/47/React.svg"/>
+            <Box component='header' className='max-w-[100vw] h-[10vh] border-b'>
+                <Link to={"/"} className='flex justify-center items-center gap-4'>
+                    <img src="/news-icon.svg" className='h-[10vh]'/>
+                    <Typography variant='button' component='p'>
+                        News Search
+                    </Typography>
                 </Link>
             </Box>
-            <Box component='main' className='max-w-[100vw] min-h-[80vh] flex flex-col gap-6 p-8 justify-center items-center'>
+            <Box 
+                component='main' 
+                className='max-w-[100vw] min-h-[80vh] flex flex-col gap-6 p-8 justify-center items-center'
+            >
                 <Routes>
                     <Route path="/" element={<Navigate to="search" replace />} />
                     <Route path="search" element={<SearchPage />} />
@@ -19,7 +25,9 @@ export default function App() {
                 </Routes>
             </Box>
             <Box component='footer' className='border-t max-w-[100vw] h-[10vh]'>
-                <p className='text-center p-4'>Copyright 2022</p>
+                <Typography variant='body1' component="p" className='text-center p-4'>
+                    Copyright 2022
+                </Typography>
             </Box>
         </BrowserRouter>
     )
